484db9e315b1bbed7a6c83691bcaefb97268053f
Deep_Roy <royd5333@gmail.com>
2023-09-21 20:07:31 +0530

    new files added

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from './components/Card.jsx'
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-7 rounded-2xl mb-6">Deep Roy</h1>
+      <Card name="Deep Roy"/>
+    </>
+  );
+}
+
+export default App;
