f39fe4b3ef652f28aff6aaac2015d6342169165c
Deep_Roy <royd5333@gmail.com>
2023-12-30 12:24:58 +0530

    added

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,13 @@
+import UserContextProvider from "./context/UserContextProvider"
+
+function App() {
+
+  return (
+    <UserContextProvider>
+    
+    <h1>Deep Roy</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
